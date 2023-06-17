468fbf02b8227f4cf101fdb49c394060ae30097a
中田　拓弥 <sputnik_u@nakatatakuwatarus-MacBook-Pro.local>
2023-06-17 10:00:07 +0900

    validate content.to_plain_text

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,21 @@ class Post < ApplicationRecord
   has_rich_text :content
 
   validates :title, presence: true , length: { maximum: 32 }
+
+  validate :validate_content_length
+
+  MAX_CONTENT_LENGTH = 100
+
+  def validate_content_length
+    length = content.to_plain_text.length
+
+    if content.to_plain_text.length > MAX_CONTENT_LENGTH
+      errors.add(
+        :content, 
+        :too_long, 
+        max_content_length: MAX_CONTENT_LENGTH,
+        length: length
+      )
+    end
+  end
 end
